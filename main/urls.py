2c116c175a40c0193271fafa0c9b878968d5e637
sergey <torenpro@gmail.com>
2017-12-17 20:40:10 +0300

    another changes

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,5 @@ from ifphotos import views
 urlpatterns = [
     re_path(r'^$', views.IndexView.as_view(), name='index'),
     re_path(r'^info/$', views.InfoView.as_view(), name='info'),
-    re_path(r'^admin/', admin.site.urls),
+    # re_path(r'^admin/', admin.site.urls),
 ]
